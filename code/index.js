f5236cd7be87f898fc38cd5c6020e36189faa172
nbuevich <nikitabuevich@gmail.com>
2016-06-14 14:29:50 +0300

    promises and first working prototype

--- code/index.js
+++ code/index.js
@@ -1,5 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api';
-import request from 'request';
+import rp from 'request-promise';
 import moment from 'moment';
 import { TELEGRAM, VK } from '../configs/token';
 
@@ -8,36 +8,45 @@ const botOptions = {
 };
 var bot = new TelegramBot(TELEGRAM.token, botOptions);
 
-bot.getMe().then(function(me) {
+bot.getMe().then(me => {
 	console.log('Hello! My name is %s!', me.first_name);
 	console.log('My id is %s.', me.id);
 	console.log('And my username is @%s.', me.username);
 });
 
-bot.on('text', function(msg) {
+bot.on('text', msg => {
 	let messageChatId = msg.chat.id;
 	let messageText = msg.text;
 	let messageDate = msg.date;
 	let messageUsr = msg.from.username;
 
-	console.log(msg);
-
 	const command = '/party ';
 
-	let response = 'Hello, ' + messageUsr;
+	console.log(msg);
+	if (messageText.includes(command)) {
+		checkDate(messageText, messageDate, command).then(date => {
+			return getGroups(date);
+		}).then(response => {
+			sendMessageByBot(messageChatId, response);
+		});
+	} else if (messageText === '/help' || messageText === '/party') {
+		sendMessageByBot(messageChatId, "Send '/party today' to find today parties. Send '/party <18 июня>' to find party in a certain day.");
+	} else {
+		sendMessageByBot(messageChatId, "Hi, " + messageUsr + "! Send /help to see what I can!");
+	}
+});
+
+function checkDate(messageText, messageDate, command) {
 	let date = getDateFromTimestamp(messageDate);
 
 	if (messageText === command + 'today') {
-
+		date = normalizeDate(date);
 	} else if (messageText.includes(command)) {
-		date = messageText.replace(command);
+		date = encodeURI(messageText.replace(command, ''));
 	}
-	date = normalizeDate(date);
-	console.log(date)
-	response = getGroups(date);
-	console.log(response)
-	sendMessageByBot(messageChatId, response);
-});
+
+	return Promise.resolve(date);
+};
 
 function getDateFromTimestamp(timestamp) {
 	return moment(timestamp*1000);
@@ -50,7 +59,6 @@ function normalizeDate(date) {
 	};
 
 	moment.locale(options.locale);
-
 	date = date.format(options.format);
 	date = encodeURI(date);
 
@@ -69,24 +77,24 @@ function getGroups(date) {
 		'country_id': 1,
 		'city_id': 119,
 		'future': 1,
-		'access_token': VK.my_token
+		'access_token': VK.access_token
 	};
 	let string = r.date;
 	let url = 'https://api.vk.com/method/' + r.method + '?q=' + string + '&future=' + r.future + '&country_id=' + r.country_id + '&city_id=' + r.city_id + '&type=' + r.type + '&access_token=' + r.access_token;
 	let my_response = "Let's party!";
 
-	request(url, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			my_response = JSON.parse(body);
-			my_response = my_response.response;
+	return rp(url).then(htmlString => {
+		my_response = JSON.parse(htmlString);
+		my_response = my_response.response;
 
-			if (my_response[0] > 0) {
-				my_response = my_response[1].name;
-			} else {
-				my_response = "No parties that day :(";
-			}
+		console.log(my_response)
+
+		if (my_response[0] > 0) {
+			return my_response[1].name;
+		} else {
+			return "No parties that day :(";
 		}
+	}).catch(err => {
+		console.log(err);
 	});
-
-	return my_response; // Проблема асинхронности. Сделать колбэки или промизы
 };
